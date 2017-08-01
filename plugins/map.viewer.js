a5aaefd2dd5448f952536d3d441422fd95183383
andrei <bulk.ba@gmail.com>
2017-08-01 20:29:03 +0300

    layout; start viewers

---
+++ plugins/map.viewer.js
@@ -0,0 +1,23 @@
+(function() {
+	Viewer.name = 'map';
+	Viewer.type = 'cover';
+	
+	Viewer.prototype.sniff = function(text) {
+		return .5;
+	}
+	
+	function Viewer() {
+		
+	}	
+	
+	Viewer.prototype.size = function() {
+		
+	}
+	
+	Viewer.prototype.draw = function(size) {
+		
+	}
+	
+	viewers.push(Viewer);
+	
+})();
---
+++ plugins/outline.viewer.js
@@ -0,0 +1,26 @@
+(function() {
+	Viewer.name = 'outline';
+	Viewer.type = 'contain';
+	
+	Viewer.prototype.sniff = function(text) {
+		return .5;
+	}
+	
+	function Viewer(dom) {
+		
+	}	
+	
+	Viewer.prototype.size = function() {
+		
+	}
+	
+	Viewer.prototype.draw = function(size) {
+		
+	}
+	
+	viewers.push(Viewer);
+	
+})();
+
+
+
